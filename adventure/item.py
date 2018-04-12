750e17fa2cb7e49552a81ecceb5ccc580e257de3
Ethan Peterson <cap2010.ep@gmail.com>
2018-04-12 00:10:45 -0600

    Read the information to be stored in map from txt file.

--- adventure/item.py
+++ adventure/item.py
@@ -6,7 +6,7 @@ class Item:
 class Chest(Item):
     ''' Chest represents the win condition for the game '''
     def __init__(self, number):
-        Item.__init__('chest')
+        Item.__init__(self, 'chest')
         self.number = number # this chest's number
 
     def unlocked_by(self, key):
@@ -17,12 +17,12 @@ class Chest(Item):
 class Key(Item):
     ''' Key represents an item that opens a chest '''
     def __init__(self, number):
-        Item.__init__('key')
+        Item.__init__(self, 'key')
         self.number = number # chest number that this key unlocks
 
 class RadiantPool(Item):
     def __init__(self):
-        Item.__init__('radiant pool')
+        Item.__init__(self, 'radiant pool')
 
     def cleanse(self, character):
         character.heal_for(100)
--- adventure/map.py
+++ adventure/map.py
@@ -1,3 +1,7 @@
+from character import Character
+from collections import defaultdict
+import item
+
 class Map:
     ''' Map represents a location for an adventurer to navigate '''
     def __init__(self, filename):
@@ -6,12 +10,12 @@ class Map:
 
     def __str__(self):
         ''' the string representation of a map is just the cell ids arranged in grid form '''
-        s = ''
+        map = ''
         for row in self.cells:
             for cell in row:
-                s += str(cell) + ' '
-            s += '\n'
-        return s
+                map += str(cell) + ' '
+            map += '\n'
+        return map
 
     def init_map(self, filename):
         ''' read_map goes to the given file and parses out map details '''
@@ -29,18 +33,68 @@ class Map:
             for col in range(len(self.cells[row])):
                 self.cells[row][col] = Cell(cell_info[col]) # instantiate a Cell with the id logged in the cell_info
 
-        self.populate_map(map_file) # populate the map with characters and items
-        map_file.close()            # close the
+        info = self.read_map_info(map_file) # get all the information to be added to the map (chars, items, etc.)
+        map_file.close()                    # close the map file
+        self.populate_map(info)             # populate the map with the infomration from the file
 
-    def populate_map(self, map_file):
+    def populate_map(self, info):
+        ''' populate_map takes the given information and adds it into the map '''
         return
+
+    def read_map_info(self, map_file):
+        ''' read_map_info reads the remainder of the map file to extract the information to be added to the map '''
+        info = defaultdict(list) # all information to be added to the map
+
+        # while there is still information, store it in the info dict
+        map_line = map_file.readline()
+        while map_line:
+            new_info = None # information to be added to the map
+
+            # separate the cell that the info relates to from the rest of the info
+            n = map_line.split(':')
+            cell_num, map_line = n[0], n[1]
+
+            # extract the type of information we are adding to the cell from the information
+            t = map_line.split('|')
+            info_type, map_line = t[0], t[1] # remove the remaining bracket for data integrity
+
+            # based on the type of the information to add, build the information
+            if info_type == 'character':
+                attrs = map_line.split()
+                damage_die = int(attrs[0])
+                hp = int(attrs[1])
+                level = int(attrs[2])
+                name = attrs[3]
+                xp = int(attrs[4])
+
+                new_info = Character(damage_die, hp, level, name, xp)
+            elif info_type == 'item':
+                attrs = map_line.split()
+                item_type = attrs[0]
+                if item_type == 'chest':
+                    new_info = item.Chest(int(attrs[1]))
+                elif item_type == 'key':
+                    new_info = item.Key(int(attrs[1]))
+                elif item_type == 'radiantpool':
+                    new_info = item.RadiantPool()
+                else:
+                    new_info = item_type
+                    return
+            else:
+                # directly add any other info
+                new_info = info_type
+
+            info[cell_num].append(new_info) # add the new information
+            map_line = map_file.readline()
+        return info
 
 class Cell:
     ''' Cell represents a single visitable location in a Map '''
     def __init__(self, id):
         self.id = id    # id of the cell
-        self.npcs = []  # characters in the cell
         self.items = [] # items in the cell
+        self.npcs = []  # characters in the cell
+        self.start = False
 
     def __str__(self):
         return self.id
@@ -64,6 +118,6 @@ class Cell:
 
 def main():
     m = Map('map1.txt')
-    print(m)
+    print(m, end='')
 
 main()
