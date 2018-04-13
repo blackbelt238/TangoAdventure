b829de9024f475fdee3634498b6a676f3104db9f
Ethan Peterson <cap2010.ep@gmail.com>
2018-04-13 09:00:30 -0600

    Create adventure skeleton with a map and a player.

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -1,1 +1,20 @@
-# make changes here
+from map import Map
+
+class Adventure:
+    def __init__(self, map_file_name, create_character):
+        self.player = None
+        self.world = Map(map_file_name) # load the given map as the world for the adventure
+        if create_character:
+            self.create_character()
+
+    def create_character(self):
+        return
+
+    def start(self):
+        ''' start kicks off an adventure '''
+        print(self.world)
+
+def main():
+    adv = Adventure('map1.txt', False)
+    adv.start()
+main()
