9fa74e6c20c24bd16ea0079cb7c51443d5cd57b1
Ethan Peterson <cap2010.ep@gmail.com>
2018-04-19 14:31:02 -0600

    Output and recieve input for possible actions from a cell.

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -3,6 +3,7 @@ from adventurer import Adventurer
 from map import Map
 
 class Adventure:
+
     def __init__(self, map_file_name, create_character):
         self.player = Adventurer('Tango', 'fighter') # default character
         self.player_x = 0                            # player's current x-coord
@@ -28,12 +29,12 @@ class Adventure:
 
     def start(self):
         ''' start kicks off an adventure '''
-        print(self.world)
-        print('Starting', self.player, 'at (' + str(self.player_x) + ',' + str(self.player_y) + ')')
+        # print(self.world)
+        # print('Starting', self.player, 'at (' + str(self.player_x) + ',' + str(self.player_y) + ')')
 
         # keep visiting locations until the move count runs out
         moves = 0
-        while moves < 20:
+        while moves < 1:
             self.visit_location()
             moves += 1
         print('You ran out of moves!')
@@ -43,16 +44,16 @@ class Adventure:
         opt = []
 
         # north
-        if not (self.player_y-1 < 0) and self.world[self.player_y-1][self.player_x] != '#':
+        if not (self.player_y-1 < 0) and self.world.cells[self.player_y-1][self.player_x].id != '#':
             opt.append('North')
         # east
-        elif not (self.player_x+1 > len(self.world[self.player_y])) and self.world[self.player_y][self.player_x+1] != '#':
+        elif not (self.player_x+1 > len(self.world.cells[self.player_y])) and self.world.cells[self.player_y][self.player_x+1].id != '#':
             opt.append('East')
         # south
-        elif not (self.player_y+1 > len(self.world)) and self.world[self.player_y+1][self.player_x] != '#':
+        elif not (self.player_y+1 > len(self.world.cells)) and self.world.cells[self.player_y+1][self.player_x].id != '#':
             opt.append('South')
         # west
-        elif not (self.player_x-1 < 0) and self.world[self.player_y][self.player_x-1] != '#':
+        elif not (self.player_x-1 < 0) and self.world.cells[self.player_y][self.player_x-1].id != '#':
             opt.append('West')
         return opt
 
@@ -61,13 +62,30 @@ class Adventure:
 
     def visit_location(self):
         ''' visit_location enables the player to visit a location '''
-        print('visiting:', self.player_x, self.player_y)
         self.visit_combat()  # combat happens immediately
         self.visit_options() # prompt user with post-combat options
 
     def visit_options(self):
         ''' visit_options presents the player with all post-combat options '''
         opt = self.travel_options() # directions player can travel from current location
+
+        # output possible directions
+        print('You see a path to the', opt[0], end='')
+        for i in range(1, len(opt)):
+            print(',', option[i], end = '')
+        print('.')
+
+        # output items
+        for i, item in enumerate(self.world.cells[self.player_y][self.player_x].items):
+            if i == 0:
+                print('On the ground in front of you is:', item, end='')
+                continue
+            print(',', item, end='')
+
+        # prompt user and take action
+        choice = input("\nWhat would you like to do? ")
+        opt.extend(self.world.cells[self.player_y][self.player_x].items) # add all items as interactable
+        self.visit_action(opt, choice)
 
 def main():
     adv = Adventure('map1.txt', False)
